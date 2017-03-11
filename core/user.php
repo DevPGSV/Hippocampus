c10fe95a6363d356d389d556acc9d6a218ef48eb
DevPGSV <DevPGSV@gmail.com>
2017-03-12 00:46:21 +0100

    Updated User

--- core/user.php
+++ core/user.php
@@ -4,7 +4,39 @@ class User {
 
     private $hc;
 
-    public function __construct($hc) {
-      $this->hc = $hc;
+    private $id;
+    private $email;
+    private $confirmedEmail;
+    private $secretToken;
+    private $role;
+
+    public function __construct($hc, $id, $email, $confirmedEmail, $secretToken, $role) {
+      $this->hc             = $hc;
+      $this->id             = $id;
+      $this->email          = $email;
+      $this->confirmedEmail = $confirmedEmail;
+      $this->secretToken    = $secretToken;
+      $this->role           = $role;
     }
+
+    public function getId() {
+      return $this->id;
+    }
+
+    public function getEmail() {
+      return $this->email;
+    }
+
+    public function isConfirmedEmail() {
+      return $this->confirmedEmail;
+    }
+
+    public function getSecretToken() {
+      return $this->secretToken;
+    }
+
+    public function getRole() {
+      return $this->role;
+    }
+
 }
