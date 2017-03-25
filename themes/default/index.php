0f73ff599c73f6e17b77d0849a412bb440a6da04
Guillermo Monserrate Sánchez <ramzafft@MacBook-Pro-de-Guillermo.local>
2017-03-25 20:06:18 +0100

    Revert "Simple login form"
    This reverts commit 5f284ed52faca2b99f121bcef50605049fff8806.

--- themes/default/index.php
+++ themes/default/index.php
@@ -1,21 +1,11 @@
 <html>
-<!-- <body>
+<head>
+  <link rel="stylesheet" type="text/css" href="<?php echo 'themes/'.$this->themeManager->getFeaturePath('style'); ?>">
+  <script src="<?php echo 'themes/'.$this->themeManager->getFeaturePath('javascript'); ?>"></script>
+</head>
+<body>
 Output - Not logged in
 <br>
 <a href="?loggedin">Log in</a>
-</body> -->
-  <head>
-      <title>Formulario de login</title>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
-      <link rel="stylesheet" href="style.css">
-  </head>
-  <body>
-    <form action="">
-      <h2>Formulario de login</h2>
-      <input type="text" placeholder="&#128272; Usuario" name="usuario">
-      <input type="text" placeholder="&#128272; Contraseña" name="password">
-      <input type="submit" value="Ingresar">
-    </form>
-  </body>
+</body>
 </html>
