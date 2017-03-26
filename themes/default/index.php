47a3d1759f9a2d3fa64b966300b90c9c97ffce66
Guillermo Monserrate Sánchez <ramzafft@MacBook-Pro-de-Guillermo.local>
2017-03-26 20:00:22 +0200

    Simple form login

--- themes/default/index.php
+++ themes/default/index.php
@@ -1,11 +1,20 @@
-<html>
+<html lang="es">
+
+<link rel="stylesheet" type="text/css" href="themes/default/css/style.css">
+<script src="themes/default/js/scripts.js"></script>
+
 <head>
-  <link rel="stylesheet" type="text/css" href="<?php echo 'themes/'.$this->themeManager->getFeaturePath('style'); ?>">
-  <script src="<?php echo 'themes/'.$this->themeManager->getFeaturePath('javascript'); ?>"></script>
+    <title>Formulario de login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+    <link rel="stylesheet" href="themes/default/css/style.css">
 </head>
 <body>
-Output - Not logged in
-<br>
-<a href="?loggedin">Log in</a>
+  <form action="">
+    <h2>Formulario de login</h2>
+    <input type="text" placeholder="&#128272; Usuario" name="usuario">
+    <input type="text" placeholder="&#128272; Contraseña" name="password">
+    <input type="submit" value="Ingresar">
+  </form>
 </body>
 </html>
