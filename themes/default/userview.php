58c33bd56ecb17c78c87243c1d05b42c2d42a010
RamzaFFT <gmonserr@ucm.es>
2017-04-02 23:21:09 +0200

    Added alternative background gradient

--- themes/default/userview.php
+++ themes/default/userview.php
@@ -1,7 +1,93 @@
 <html>
+<head>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="themes/default/css/style.css">
+    <script src="themes/default/js/scripts.js"></script>
+
+      <title>Formulario de login</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1">
+
+
+  <script src="themes/"></script>
+</head>
+
+<body id="user">
+
+
+  <div class="page">
+    <div class="wrapper">
+      <div class="content-wrapper">
+        <div class="content">
+          <h1>Content</h1>
+          <p>&larr; Responsive Width &rarr;</p>
+        </div>
+      </div>
+      <div class="sidebar">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+
+
 <body>
-Output - Logged in as <?php echo $u->getEmail(); ?>
-<br>
-<a href="?">Log out</a>
+
+<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+
+<nav class="navbar navbar-default">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#barraBasica">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Site</a>
+					</div>
+
+					<div class="collapse navbar-collapse" id="barraBasica">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+							<li><a href="#"> Portfólio </a></li>
+							<li><a href="#"> Matérias </a></li>
+							<li><a href="#"> Contato </a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+
+		<!--<script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>-->
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <h2>Page sidebar</h2>
+        <p>&larr; Fixed Width &rarr;</p>
+      </div>
+    </div>
+  </div>
+
+<!--<div class="main">
+<div class="top">TOP
+  Output - Logged in as <?php echo $u->getEmail(); ?>
+  <br>
+  <a href="?">Log out</a>
+</div>
+<div class="left">MIS APLICACIONES</div>
+<div class="right">MAIN</div>
+<div class="clear"></div>
+</div>-->
+
 </body>
+
 </html>
