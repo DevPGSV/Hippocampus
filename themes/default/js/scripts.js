26f06f493c0207cb500cae044588c6256a9c29ae
DevPGSV <DevPGSV@gmail.com>
2017-04-12 00:05:29 +0200

    Enabled again dynamic background

--- themes/default/js/scripts.js
+++ themes/default/js/scripts.js
@@ -41,7 +41,7 @@ function updateGradient() {
 	var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
 	var color2 = "rgb("+r2+","+g2+","+b2+")";
 
-	$('body2').css({
+	$('body').css({
 		background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
 			background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
 
