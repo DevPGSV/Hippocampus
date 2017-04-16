0364adfd518f1f12ca00d60b247e0d57c6d5d8e9
Marta Pastor Puente <marta.pastor@live.com>
2017-04-16 22:06:16 +0200

    Changed general font to use a customised one.

--- themes/default/admin.php
+++ themes/default/admin.php
@@ -14,6 +14,7 @@
 	<head>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<link rel='stylesheet' href='//fonts.googleapis.com/css?family=Actor'>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 		<link rel="stylesheet" type="text/css" href="themes/default/css/style.css">
