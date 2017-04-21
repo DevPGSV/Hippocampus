757e23509fbcbcc771c1ce94c9eaf7e7e1d0d75d
bldelato <bldelato@ucm.es>
2017-04-21 14:07:19 +0200

    Modified Main image

--- themes/default/userview.php
+++ themes/default/userview.php
@@ -80,7 +80,7 @@
       </div>
 
       <div id="logo" class="container-fluid">
-        <a href="http://imgur.com/tV6e0n7"><img id="img" src="http://i.imgur.com/tV6e0n7.png" title="source: imgur.com" /></a>
+        <a href="http://imgur.com/yJuHskd"><img id="img" src="http://i.imgur.com/yJuHskd.png" title="source: imgur.com" /></a>
       </div>
 
       <div class="container-fluid">
@@ -106,7 +106,7 @@
 
                     <ul class="nav navbar-nav navbar-right">
                       <li><a data-toggle="pill" href="#menu1"><span class="glyphicon glyphicon-plus"></span></a></li>
-                      
+
                       <!-- MENU NOTIFICACIONES -->
                       <li><div class="dropdown" id="div-menu2"><button class="btn btn-default dropdown-toggle" type="button" id="menu2" data-toggle="dropdown"><span class="glyphicon glyphicon-bullhorn"><span class="badge">4</span></span></button>
                         <ul class="dropdown-menu" role="menu">
@@ -114,9 +114,11 @@
                           <li role="presentation " class="notification-text-inside"><svg class="notification-icon bolotweet"><use xlink:href="#bolotweet"></use></svg><a role="menuitem" tabindex="-1" href="#" class="notification-text">  Tienes 1 mensaje nuevo.</a></li>
                           <li role="presentation"><svg class="notification-icon ucm"><use xlink:href="#ucm"></use></svg><a role="menuitem" tabindex="-1" href="#" class="notification-text">    Tienes 2 nuevos mensajes.</a></li>
                         </ul></div></li>
-                        <!-- END MENU NOTIFICACIONES -->
+                      <!-- END MENU NOTIFICACIONES -->
 
+                      <!-- MENU PERFIL -->
                       <li><a data-toggle="pill" href="#menu3"><span class="glyphicon glyphicon-user" title="<?php echo $u->getUsername(); ?>"></span></a></li>
+                      <!-- END MENU PERFIL -->
                       <li><a href="logout"><span class="glyphicon glyphicon-off" title="Log out"></span></a></li>
                     </ul>
                 </div>
@@ -133,74 +135,77 @@
                         <p></p>
                     </div>
 
-                      <div id="menu3" class="tab-pane fade">
+                    <!-- MENU PERFIL -->
+                     <div id="menu3" class="tab-pane fade">
 
-                      <div class="container">
-                          <h1>Editar tu perfil</h1>
-                        	<hr>
-                      	<div class="row">
-                            <!-- left column -->
-                            <div class="col-md-3">
-                              <div class="text-center">
-                                <a href="http://imgur.com/cTCsZeR"><img src="http://i.imgur.com/cTCsZeR.png" title="source: imgur.com" /></a>
-                                <!--<h6>Sube una nueva imagen</h6>
+                     <div class="container">
+                         <h1>Editar tu perfil</h1>
+                         <hr>
+                       <div class="row">
+                           <!-- left column -->
+                           <div class="col-md-3">
+                             <div class="text-center">
+                               <a href="http://imgur.com/cTCsZeR"><img src="http://i.imgur.com/cTCsZeR.png" title="source: imgur.com" /></a>
+                               <!--<h6>Sube una nueva imagen</h6>
 
-                                <input class="form-control" type="file">-->
-                              </div>
-                            </div>
+                               <input class="form-control" type="file">-->
+                             </div>
+                           </div>
 
-                            <!-- edit form column -->
-                              <div class="col-md-9 personal-info">
-                                <!--<div class="alert alert-info alert-dismissable">
-                                  <a class="panel-close close" data-dismiss="alert">×</a>
-                                  <i class="fa fa-coffee"></i>
-                                   Puedes cambiar tus datos desde el menú de Administración
-                                 </div>-->
-                                <div id="alerta"><h3>Información de la cuenta</h3></div>
+                           <!-- edit form column -->
+                             <div class="col-md-9 personal-info">
+                               <!--<div class="alert alert-info alert-dismissable">
+                                 <a class="panel-close close" data-dismiss="alert">×</a>
+                                 <i class="fa fa-coffee"></i>
+                                  Puedes cambiar tus datos desde el menú de Administración
+                                </div>-->
+                               <div id="alerta"><h3>Información de la cuenta</h3></div>
 
-                                <form class="form-horizontal" role="form">
-                                  <div class="form-group">
-                                    <label class="col-lg-3 control-label">Nombre</label>
-                                    <div class="col-lg-8">
-                                      <input class="form-control" value="User" type="text">
-                                    </div>
-                                  </div>
-                                  <div class="form-group">
-                                    <label class="col-lg-3 control-label">Apellidos</label>
-                                    <div class="col-lg-8">
-                                      <input class="form-control" value="Lastname" type="text">
-                                    </div>
-                                  </div>
-                                  <div class="form-group">
-                                    <label class="col-lg-3 control-label">Correo electrónico</label>
-                                    <div class="col-lg-8">
-                                      <input class="form-control" value="user@gmail.com" type="text">
-                                    </div>
-                                  </div>
-                                  <div class="form-group">
-                                    <label class="col-md-3 control-label">Nombre de usuario</label>
-                                    <div class="col-md-8">
-                                      <input class="form-control" value="username" type="text">
-                                    </div>
-                                  </div>
-                                  <div class="form-group">
-                                    <label class="col-md-3 control-label">Contraseña</label>
-                                    <div class="col-md-8">
-                                      <input class="form-control" value="11111122333" type="password">
-                                    </div>
-                                  </div>
-                                  <div class="form-group">
-                                    <label class="col-md-3 control-label">Confirmar contraseña</label>
-                                    <div class="col-md-8">
-                                      <input class="form-control" value="11111122333" type="password">
-                                    </div>
-                                  </div>
-                                </form>
-                              </div>
-                          </div>
-                        </div>
-                        <hr>
-                      </div>
+                               <form class="form-horizontal" role="form">
+                                 <div class="form-group">
+                                   <label class="col-lg-3 control-label">Nombre</label>
+                                   <div class="col-lg-8">
+                                     <input class="form-control" value="User" type="text">
+                                   </div>
+                                 </div>
+                                 <div class="form-group">
+                                   <label class="col-lg-3 control-label">Apellidos</label>
+                                   <div class="col-lg-8">
+                                     <input class="form-control" value="Lastname" type="text">
+                                   </div>
+                                 </div>
+                                 <div class="form-group">
+                                   <label class="col-lg-3 control-label">Correo electrónico</label>
+                                   <div class="col-lg-8">
+                                     <input class="form-control" value="user@gmail.com" type="text">
+                                   </div>
+                                 </div>
+                                 <div class="form-group">
+                                   <label class="col-md-3 control-label">Nombre de usuario</label>
+                                   <div class="col-md-8">
+                                     <input class="form-control" value="username" type="text">
+                                   </div>
+                                 </div>
+                                 <div class="form-group">
+                                   <label class="col-md-3 control-label">Contraseña</label>
+                                   <div class="col-md-8">
+                                     <input class="form-control" value="11111122333" type="password">
+                                   </div>
+                                 </div>
+                                 <div class="form-group">
+                                   <label class="col-md-3 control-label">Confirmar contraseña</label>
+                                   <div class="col-md-8">
+                                     <input class="form-control" value="11111122333" type="password">
+                                   </div>
+                                 </div>
+                               </form>
+                             </div>
+                         </div>
+                       </div>
+                       <hr>
+                     </div>
+                     <!-- END MENU PERFIL -->
+
                     </div>
               </div>
             </nav>
