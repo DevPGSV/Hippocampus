89f131ecc557f1eae14d1ad2b6953b0814dc0213
bldelato <bldelato@ucm.es>
2017-04-24 23:06:41 +0200

    Modified button for Profile

--- themes/default/userview.php
+++ themes/default/userview.php
@@ -137,8 +137,7 @@
 
                 <div class="collapse navbar-collapse" id="barraBasica">
                     <ul class="nav navbar-nav">
-                        <li><a href="#"  class=" navbar-element navbar-main-title">Home</a></li> <!-- HE QUITADO ACTIVE Y <a href="#">-->
-                        <!--<li><a href="#">Aplicaciones</a></li>-->
+                        <li><a href="#"  class=" navbar-element navbar-main-title">Home</a></li> <!-- HE QUITADO ACTIVE -->
                         <li><a href="admin" class="navbar-element navbar-main-title">Administración</a></li>
                     </ul>
 
@@ -165,86 +164,84 @@
                       <!-- END MENU NOTIFICACIONES -->
 
                       <!-- MENU PERFIL -->
-                      <li><a data-toggle="pill" href="#menu3"><span class="glyphicon glyphicon-user navbar-element" title="<?php echo $u->getUsername(); ?>"></span></a></li>
+                      <li><div class="dropdown" id="div-menu3"><button class="btn btn-default dropdown-toggle" type="button" id="menu3" data-toggle="dropdown"><span class="glyphicon glyphicon-user navbar-element"></span></button>
+                        <ul class="dropdown-menu" role="menu" id="perfil-menu">
+                          <div class="container">
+                              <h3>Editar tu perfil</h3>
+                            <div class="row">
+                                <!-- left column -->
+                                <div class="col-md-3" id="pic-column">
+                                  <div class="text-center">
+                                    <img src="http://i.imgur.com/cTCsZeR.png" id="foto-perfil">
+                                    <h6>Sube una nueva imagen</h6>
+                                    <!--<input class="form-control" type="file">-->
+                                    <div class="input-group" id="perfil-foto-input">
+            													<label class="input-group-btn">
+            														 <span class="btn btn-primary">
+            																 Buscar&hellip; <input type="file" style="display: none;" multiple>
+            														 </span>
+            												 	</label>
+            												 <input type="text" class="form-control"  accept="image/*">
+                                   </div>
+                                  </div>
+                                </div>
+
+                                <!-- edit form column -->
+                                  <div class="col-md-9 personal-info" id="info-column">
+                                    <!--<div class="alert alert-info alert-dismissable">
+                                      <a class="panel-close close" data-dismiss="alert">×</a>
+                                      <i class="fa fa-coffee"></i>
+                                       Puedes cambiar tus datos desde el menú de Administración
+                                     </div>
+                                    <div id="alerta"><h3>Información de la cuenta</h3></div>-->
+
+                                    <form class="form-horizontal" role="form">
+                                      <div class="form-group part-perfil">
+                                        <label class="col-lg-3 control-label simple-part">Nombre</label>
+                                        <div class="col-lg-8">
+                                          <input class="form-control" value="Usuario" type="text">
+                                        </div>
+                                      </div>
+                                      <div class="form-group part-perfil">
+                                        <label class="col-lg-3 control-label simple-part">Apellidos</label>
+                                        <div class="col-lg-8">
+                                          <input class="form-control" value="Apellido" type="text">
+                                        </div>
+                                      </div>
+                                      <div class="form-group part-perfil">
+                                        <label class="col-lg-3 control-label complex-part">Correo electrónico</label>
+                                        <div class="col-lg-8">
+                                          <input class="form-control" value="<?php echo $u->getEmail(); ?>" type="text">
+                                        </div>
+                                      </div>
+                                      <div class="form-group part-perfil">
+                                        <label class="col-md-3 control-label complex-part">Nombre de usuario</label>
+                                        <div class="col-md-8">
+                                          <input class="form-control" value="<?php echo $u->getUsername(); ?>" type="text">
+                                        </div>
+                                      </div>
+                                      <div class="form-group part-perfil">
+                                        <label class="col-md-3 control-label simple-part">Contraseña</label>
+                                        <div class="col-md-8">
+                                          <input class="form-control" value="" type="password">
+                                        </div>
+                                      </div>
+                                      <div class="form-group part-perfil">
+                                        <label class="col-md-3 control-label complex-part">Confirmar contraseña</label>
+                                        <div class="col-md-8">
+                                          <input class="form-control" value="" type="password">
+                                        </div>
+                                      </div>
+                                    </form>
+                                  </div>
+                              </div>
+                            </div>
+                        </ul></div></li>
                       <!-- END MENU PERFIL -->
                       <li><a href="logout"><span class="glyphicon glyphicon-off navbar-element" title="Log out"></span></a></li>
                     </ul>
                 </div>
 
-                <div class="tab-content">
-                  
-                    <!-- MENU PERFIL -->
-                     <div id="menu3" class="tab-pane fade">
-
-                     <div class="container">
-                         <h1>Editar tu perfil</h1>
-                         <hr>
-                       <div class="row">
-                           <!-- left column -->
-                           <div class="col-md-3">
-                             <div class="text-center">
-                               <img src="http://i.imgur.com/cTCsZeR.png">
-                               <!--<h6>Sube una nueva imagen</h6>
-
-                               <input class="form-control" type="file">-->
-                             </div>
-                           </div>
-
-                           <!-- edit form column -->
-                             <div class="col-md-9 personal-info">
-                               <!--<div class="alert alert-info alert-dismissable">
-                                 <a class="panel-close close" data-dismiss="alert">×</a>
-                                 <i class="fa fa-coffee"></i>
-                                  Puedes cambiar tus datos desde el menú de Administración
-                                </div>-->
-                               <div id="alerta"><h3>Información de la cuenta</h3></div>
-
-                               <form class="form-horizontal" role="form">
-                                 <div class="form-group">
-                                   <label class="col-lg-3 control-label">Nombre</label>
-                                   <div class="col-lg-8">
-                                     <input class="form-control" value="User" type="text">
-                                   </div>
-                                 </div>
-                                 <div class="form-group">
-                                   <label class="col-lg-3 control-label">Apellidos</label>
-                                   <div class="col-lg-8">
-                                     <input class="form-control" value="Lastname" type="text">
-                                   </div>
-                                 </div>
-                                 <div class="form-group">
-                                   <label class="col-lg-3 control-label">Correo electrónico</label>
-                                   <div class="col-lg-8">
-                                     <input class="form-control" value="<?php echo $u->getEmail(); ?>" type="text">
-                                   </div>
-                                 </div>
-                                 <div class="form-group">
-                                   <label class="col-md-3 control-label">Nombre de usuario</label>
-                                   <div class="col-md-8">
-                                     <input class="form-control" value="<?php echo $u->getUsername(); ?>" type="text">
-                                   </div>
-                                 </div>
-                                 <div class="form-group">
-                                   <label class="col-md-3 control-label">Contraseña</label>
-                                   <div class="col-md-8">
-                                     <input class="form-control" value="" type="password">
-                                   </div>
-                                 </div>
-                                 <div class="form-group">
-                                   <label class="col-md-3 control-label">Confirmar contraseña</label>
-                                   <div class="col-md-8">
-                                     <input class="form-control" value="" type="password">
-                                   </div>
-                                 </div>
-                               </form>
-                             </div>
-                         </div>
-                       </div>
-                       <hr>
-                     </div>
-                     <!-- END MENU PERFIL -->
-
-                    </div>
               </div>
             </nav>
           </div>
@@ -270,106 +267,6 @@
           </div>
       </div>
   </div>
-  <?php /*
-  <div class="page">
-    <div class="wrapper">
-      <!--<div class="content-wrapper">
-        <div class="content">
-          <h1>Content</h1>
-          <p>&larr; Responsive Width &rarr;</p>
-        </div>
-      </div>-->
-
-       <div class="sidebar">
-
-         <!--<nav id="main-nav" class="main-nav">
-           <ul>
-             <li class="home">
-               <a href="/"><
-                 <svg class="icon-nav-home" width="26px" height="26px">
-                   <use xlink:href="#icon-nav-home"></use>
-                 </svg>
-                 <span>Home</span>
-               </a>
-             </li>
-             <li class="videos">
-               <a href="/"><
-                 <svg class="icon-nav-video" width="26px" height="26px">
-                   <use xlink:href="#icon-nav-video"></use>
-                 </svg>
-                 <span>Home</span>
-               </a>
-             </li>
-           </ul>
-         </nav> -->
-
-            <div id="sidebar-wrapper">
-                <ul id="sidebar_menu" class="sidebar-nav">
-                  <li class="sidebar-brand"><a id="menu-toggle" href="#"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span></a></li>
-                </ul>
-                <ul class="sidebar-nav" id="sidebar">
-                  <li><a>Algo<span class="glyphicon glyphicon-user"></span></a></li>
-                  <ul class="sidebar-nav" id="sidebar">
-                    <li><a>Algo<span class="glyphicon glyphicon-search"></span></a></li>
-                    <li><a>Algo<span class="glyphicon glyphicon-heart"></span></a></li>
-                  </ul>
-                  <li><a>Algo<span class="glyphicon glyphicon-music"></span></a></li>
-                  <li><a>Algo<span class="glyphicon glyphicon-list-alt"></span></a></li>
-                  <li><a>Algo<span class="glyphicon glyphicon-envelope"></span></a></li>
-               </ul>
-            </div>
-
-             <!-- This ends new -->
-
-              <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#barraBasica">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Site</a>
-                    </div>
-
-                    <div class="collapse navbar-collapse" id="barraBasica">
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a href="#"> Home <span class="sr-only">(current)</span></a></li>
-                            <li><a href="#"> Aplicaciones </a></li>
-                            <li><a href="#"> Administración </a></li>
-                        </ul>
-                        <ul class="nav navbar-nav navbar-right">
-                          <li><a href="#"><span class="glyphicon glyphicon-plus"></span></a></li>
-                          <li><a href="#"><span class="glyphicon glyphicon-bullhorn"></span></a></li>
-                          <li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
-                        </ul>
-                    </div>
-                </div>
-              </nav>
-
-                <!--<script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>-->
-              <div id="content">
-                  <div class="column"></div>
-                  <div class="column"></div>
-                  <div class="column"></div>
-              </div>
-                <!-- Latest compiled and minified JavaScript -->
-      </div>
-    </div>
-  </div>
-
-<!--<div class="main">
-<div class="top">TOP
-  Output - Logged in as <?php echo $u->getEmail(); ?>
-  <br>
-  <a href="?">Log out</a>
-</div>
-<div class="left">MIS APLICACIONES</div>
-<div class="right">MAIN</div>
-<div class="clear"></div>
-</div>-->
- */ ?>
 </body>
 
 </html>
