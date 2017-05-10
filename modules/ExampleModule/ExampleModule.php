7468943dae8aca0c5a7e4c4c4d6d12cd9fdabbaa
DevPGSV <DevPGSV@gmail.com>
2017-05-10 07:55:50 +0200

    Added ExampleModule with basic code to use functionalities available to modules

---
+++ modules/ExampleModule/ExampleModule.php
@@ -0,0 +1,48 @@
+<?php
+
+class ExampleModule extends HC_Module {
+  public function __construct($hc) {
+    parent::__construct($hc);
+
+    $this->registerWindowCallback('example', 'ExampleWindowCallback');
+    $this->registerWindowCallback('example2', 'ExampleWindowCallback2');
+  }
+
+  public function onCreatingSidebar(&$sidebar) {
+    $newEntry = [
+      'icon' => 'twitter',
+      'text' => 'Example',
+      'id' => 'example',
+    ];
+    array_unshift($sidebar, $newEntry); // To prepend the entry
+  }
+
+  public function onCreatingNotifications(&$notifications) {
+    $newEntry = [
+      'notificationCounter' => 2,
+      'text' => 'Tienes {COUNTER} mensajes nuevos',
+      'cb' => 'ExampleNotificationCallback',
+      'cbData' => [],
+    ];
+    array_unshift($notifications, $newEntry); // To prepend the entry
+  }
+
+  public function ExampleWindowCallback() {
+    return [
+      'html' => '<p>Module dummy data for service: <em>Example</em></p><p data-updatewindowboxservice="example2">More....</p>',
+      'title' => 'Example Title',
+    ];
+  }
+
+  public function ExampleWindowCallback2() {
+    return [
+      'html' => '<p>Second callback!</p><p data-updatewindowboxservice="example">Back....</p>',
+      'title' => 'Example Second Title',
+    ];
+  }
+
+  public function ExampleNotificationCallback($cbData) {
+    return '<p>Module dummy data for notification: <em>Example</em></p><br><pre>'.print_r($cbData, true).'</pre>';
+  }
+
+}
