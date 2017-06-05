4592434d041fc82f13b541192063ff877ede27d8
RamzaFFT <gmonserr@ucm.es>
2017-06-05 20:29:16 +0200

    Added white and documentation links to repo and wiki

--- themes/default/documentation.php
+++ themes/default/documentation.php
@@ -11,8 +11,8 @@
 
   <a href="index.php" id="whoHome"><span class="glyphicon glyphicon-circle-arrow-left"></span> HOME </a>
 
-  <a class="button" id="memoria" href=""> Memoria <span class="glyphicon glyphicon-duplicate"></span> </a>  <a href="" id="Github">Github</a>
-  <a class="button" id="github" href=""> Github <span class="glyphicon glyphicon-tasks"></span> </a>
+  <a class="button" id="memoria" href="https://github.com/DevPGSV/Hippocampus/wiki"> Memoria <span class="glyphicon glyphicon-duplicate"></span> </a>  <a href="" id="Github">Github</a>
+  <a class="button" id="github" href="https://github.com/DevPGSV/Hippocampus"> Github <span class="glyphicon glyphicon-tasks"></span> </a>
   </div>
 </body>
 </html>
