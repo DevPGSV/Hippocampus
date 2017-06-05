8966265b96de2ab29c33177f88d4871bc9e62c27
Marta Pastor Puente <marta.pastor@live.com>
2017-06-05 13:47:41 +0200

    Updated Avisos module to show notifications dynamically

--- modules/AvisosModule/AvisosModule.php
+++ modules/AvisosModule/AvisosModule.php
@@ -22,25 +22,39 @@ class AvisosModule extends HC_Module {
             'text' => 'Tienes {COUNTER} mensajes nuevos',
             'cb' => 'AvisosNotificationCallback',
             'cbData' => [],
+            'icon' => 'notifications',
         ];
         array_unshift($notifications, $newEntry); // To prepend the entry
     }
 
     public function AvisosWindowCallback() {
-        // Se muestra una lista de los servicios que tienen notificaciones pendientes por leer:
 
-        $html = '
-            <svg class="notification-icon gmail"><use xlink:href="#gmail"></use></svg> Tienes 1 mensaje nuevo. </a>
-            <br>
-            <br>
-            <svg class="notification-icon bolotweet"><use xlink:href="#bolotweet"></use></svg> Tienes 2 mensajes nuevo. </a>
-            <br>
-            <br>
-            <svg class="notification-icon ucm"><use xlink:href="#ucm"></use></svg> Tienes 1 mensaje nuevo. </a>
-        ';
+        $notifications = $this->hc->getNotifications();
+        $notificationsNumber = 0;
+
+        foreach ($notifications as $notification) {
+          if (!empty($notification['notificationCounter'])) {
+            $notificationsNumber += $notification['notificationCounter'];
+          }
+        }
+
+        $avisosHtmlFormatted = "Tienes ".$notificationsNumber." notificaciones nuevas.<br><br>";
+
+        foreach ($notifications as $notification) {
+          if (!empty($notification['notificationCounter'])) {
+            $notification['text'] = str_replace('{COUNTER}', (string)$notification['notificationCounter'], $notification['text']);
+          }
+
+          $icon = '';
+          if (!empty($notification['icon'])) {
+              $icon = $notification['icon'];
+          }
+
+          $avisosHtmlFormatted .= "<p class='notification-text-inside'><svg class='notification-icon".$icon." mediumicon'><use xlink:href='#$icon'></use></svg><br>{$notification['text']}</p><br>";
+        }
 
         return [
-            'html' => $html,
+            'html' => $avisosHtmlFormatted,
             'title' => '<svg class="icon notifications windowicon">
                 <use xlink:href="#notifications">
                 </use>
@@ -50,5 +64,9 @@ class AvisosModule extends HC_Module {
 
     public function AvisosNotificationCallback($cbData) {
         return '<p> </p>';
+    }
+
+    public function onCreatingMetacode(&$metacode) {
+        $metacode[] = '<link rel="stylesheet" href="modules/AvisosModule/style.css">';
     }
 }
